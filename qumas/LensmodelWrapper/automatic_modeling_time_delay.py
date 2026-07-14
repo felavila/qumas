e19da3c9a6974daaa376d0fd0bebfce7cc664337
Felipe A. <felipeavila41@gmail.com>
2026-07-14 19:39:05 -0400

    .

--- qumas/LensmodelWrapper/automatic_modeling_time_delay.py
+++ qumas/LensmodelWrapper/automatic_modeling_time_delay.py
@@ -19,13 +19,8 @@ def set_up_dir(path_for_models,dir_models,system_name):
     return os.path.join(path_for_models,dir_models,system_name)
 # To do change all the save and reading to pickle
 
-<<<<<<< HEAD
 def automatic_modeling(system,max_separation=0.01,path_for_models=None,dir_models=None,model_All=False,do_mcmc=False,max_lap=10,
                     relative_flux_error=0.2,use_informed_flux=False,use_real_error_flux=False,do_model_of=None,time_delay=None,time_delay_error=None,center_mass_error = 0.003,astrometry_error = 0.003,**kwargs):
-=======
-def automatic_modeling(system,max_separation=0.01,path_for_models="models",dir_models="model",model_All=False,do_mcmc=False,max_lap=10,
-                    relative_flux_error=0.2,use_informed_flux=False,use_real_error_flux=False,do_model_of=None,time_delay=None,time_delay_error=None,**kwargs):
->>>>>>> 4108c68 (added mac)
     #Check for existing results
     #path_model_result_csv = os.path.join(set_up_dir(system_name,path_for_models,dir_models),"final_result",f"models_{system_name}.csv")
     system_name = system.name.values[0]
